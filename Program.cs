bf1a14186e631dc583ce8693a76294689b8255ec
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-09-26 14:14:05 -0300

    Adicionando verificação caso seja informado um valor diferente do esperado.

--- Program.cs
+++ Program.cs
@@ -9,9 +9,10 @@ Console.WriteLine();
 Console.WriteLine("Tecle 1 para Exibir detalhes do pagamento");
 Console.WriteLine("Tecle 2 para Fazer uma simulação de Férias");
 Console.WriteLine("Tecle 3 para Calcular 13º");
-Console.WriteLine("Tecle 1 para Simular Rescisão de contrato");
+Console.WriteLine("Tecle 4 para Simular Rescisão de contrato");
 Console.WriteLine();
 int opcaoEscolhida = Convert.ToInt16(Console.ReadLine());
+if(opcaoEscolhida < 1 || opcaoEscolhida > 4) Console.WriteLine("Opção inválida!");
 
 switch (opcaoEscolhida)
 {
